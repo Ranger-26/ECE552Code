8c609abceb70224fd4ea405396d3afd4d4361c8c
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2026-02-26 00:40:49 -0600

    hotfix store aligner

--- code/rtl/dmem_wdata_aligner.v
+++ code/rtl/dmem_wdata_aligner.v
@@ -5,12 +5,12 @@ module dmem_wdata_aligner (
     input wire [31:0] mem_wdata,
     output wire [31:0] mem_wdata_aligned
 );
-    assign mem_wdata_aligned = (mem_mask == 4'b0001) ? {24'b0, mem_wdata[7:0]} :
-        (mem_mask == 4'b0010) ? {16'b0, mem_wdata[15:8], 8'b0} :
-        (mem_mask == 4'b0100) ? {8'b0, mem_wdata[23:16], 16'b0} :
-        (mem_mask == 4'b1000) ? {mem_wdata[31:24], 24'b0} :
-        (mem_mask == 4'b0011) ? {16'b0, mem_wdata[15:0]} :
-        (mem_mask == 4'b1100) ? {mem_wdata[31:16], 16'b0} :
+    assign mem_wdata_aligned = (mem_mask == 4'b0001) ? {{24{mem_wdata[7]}}, mem_wdata[7:0]} :
+        (mem_mask == 4'b0010) ? {{16{mem_wdata[15]}}, mem_wdata[7:0], 8'b0} :
+        (mem_mask == 4'b0100) ? {{8{mem_wdata[23]}}, mem_wdata[7:0], 16'b0} :
+        (mem_mask == 4'b1000) ? {mem_wdata[7:0], 24'b0} :
+        (mem_mask == 4'b0011) ? {{16{mem_wdata[15]}}, mem_wdata[15:0]} :
+        (mem_mask == 4'b1100) ? {mem_wdata[15:0], 16'b0} :
         mem_wdata[31:0];
 
 endmodule
