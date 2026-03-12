bab3460c042e291c4d4b7110eb5129190efe228e
Adin Lieblein <adinmlieblein@gmail.com>
2026-03-12 08:26:09 -0500

    hazard logic

--- code/rtl/hazard_detector.v
+++ code/rtl/hazard_detector.v
@@ -26,7 +26,7 @@ module hazard_detector (
   wire adjacent_hazard = ((IF_ID_rs1 == ID_EX_write_reg) & (IF_ID_rs1 != 0)) | ((IF_ID_rs2 == ID_EX_write_reg) & ((ID_format == R_TYPE) | (ID_format == B_TYPE)) & (IF_ID_rs2 != 0));
   wire separated_hazard = ((IF_ID_rs1 == EX_MEM_write_reg) & (IF_ID_rs1 != 0)) | ((IF_ID_rs2 == EX_MEM_write_reg) & ((ID_format == R_TYPE) | (ID_format == B_TYPE)) & (IF_ID_rs2 != 0));
 
-  assign flush_IF_ID = ID_control_flow; // stall fetch -> nop into decode (IF/ID reg)
+  assign flush_IF_ID = ID_control_flow & (!flush_ID_EX); // stall fetch -> nop into decode (IF/ID reg)
   assign flush_ID_EX = adjacent_hazard | separated_hazard; // stall decode -> nop into execute (ID/EX reg)
   assign stall_pc = flush_IF_ID | flush_ID_EX;
 endmodule
