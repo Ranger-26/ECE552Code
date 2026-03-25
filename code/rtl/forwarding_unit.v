56925d6d1d81b1803c9c6aa1bb539d14c114e274
jslambert <145697055+jslambert@users.noreply.github.com>
2026-03-25 03:09:02 -0500

    Only do forwarding for rs2 ALU input for R, B, or S-type instructions

--- code/rtl/forwarding_unit.v
+++ code/rtl/forwarding_unit.v
@@ -9,6 +9,8 @@ module forwarding_unit (
   input wire [4:0] MEM_WB_write_reg,
   input wire MEM_WB_wen,
 
+  input wire [5:0] format,
+
   output wire [1:0] forward_A,
   output wire [1:0] forward_B
 
@@ -17,18 +19,15 @@ module forwarding_unit (
   // 00 = No Forwarding, 01 = EX-to-EX Forwarding, 10 = MEM-to-EX Forwarding
 
   // ALU Input 1
-  assign forward_A = (EX_MEM_wen && (EX_MEM_write_reg != 0) && (EX_MEM_write_reg == ID_EX_rs1)) ? 2'b01 : // EX-to-EX Forwarding
-                     (MEM_WB_wen && (MEM_WB_write_reg != 0) && (MEM_WB_write_reg == ID_EX_rs1)) ? 2'b10 : // MEM-to-EX Forwarding
+  assign forward_A = (EX_MEM_wen & (EX_MEM_write_reg != 0) & (EX_MEM_write_reg == ID_EX_rs1)) ? 2'b01 : // EX-to-EX Forwarding
+                     (MEM_WB_wen & (MEM_WB_write_reg != 0) & (MEM_WB_write_reg == ID_EX_rs1)) ? 2'b10 : // MEM-to-EX Forwarding
                      2'b00; // No Forwarding
 
   // ALU Input 2
-  assign forward_B = (EX_MEM_wen && (EX_MEM_write_reg != 0) && (EX_MEM_write_reg == ID_EX_rs2)) ? 2'b01 : // EX-to-EX Forwarding
-                     (MEM_WB_wen && (MEM_WB_write_reg != 0) && (MEM_WB_write_reg == ID_EX_rs2)) ? 2'b10 : // MEM-to-EX Forwarding
+  wire inst_contains_rs2 = (format == 6'b000001) | (format == 6'b000100) | (format == 6'b001000); // Only do forwarding for rs2 if instruction format is R, S, or B (any format that contains rs2)
+
+  assign forward_B = (EX_MEM_wen & (EX_MEM_write_reg != 0) & (EX_MEM_write_reg == ID_EX_rs2) & inst_contains_rs2) ? 2'b01 : // EX-to-EX Forwarding
+                     (MEM_WB_wen & (MEM_WB_write_reg != 0) & (MEM_WB_write_reg == ID_EX_rs2) & inst_contains_rs2) ? 2'b10 : // MEM-to-EX Forwarding
                      2'b00; // No Forwarding
-
-
-    
-    //for load to use stall, we need to check if the  id_ex_format is a load and if if_id_rs1 or if_id_rs2 is equal to the load register in execute 
-
-    //load word and store word combo stall???
+                     
 endmodule
