bf152437d84aa32e4acd106bc47d7ef18a42a702
Siddhu Mohan <61168189+Ranger-26@users.noreply.github.com>
2026-04-07 01:11:38 -0500

    Start on stalling, PC is fucked and is x

--- code/rtl/hart.v
+++ code/rtl/hart.v
@@ -11,23 +11,45 @@ module hart #(
     input  wire        i_rst,
     // Instruction fetch goes through a read only instruction memory (imem)
     // port. The port accepts a 32-bit address (e.g. from the program counter)
-    // per cycle and combinationally returns a 32-bit instruction word. This
-    // is not representative of a realistic memory interface; it has been
-    // modeled as more similar to a DFF or SRAM to simplify phase 3. In
-    // later phases, you will replace this with a more realistic memory.
+    // per cycle and sequentially returns a 32-bit instruction word. For
+    // projects 6 and 7, this memory has been updated to be more realistic
+    // - reads are no longer combinational, and both read and write accesses
+    // take multiple cycles to complete.
     //
+    // The testbench memory models a fixed, multi cycle memory with partial
+    // pipelining. The memory will accept a new request every N cycles by
+    // asserting `mem_ready`, and if a request is made, the memory perform
+    // the request (read or write) after M cycles, asserting mem_valid to
+    // indicate the read data is ready (or the write is complete). Requests
+    // are completed in order. The values of N and M are deterministic, but
+    // may change between test cases - you must design your CPU to work
+    // correctly by looking at `mem_ready` and `mem_valid` rather than
+    // hardcoding a latency assumption.
+    //
+    // Indicates that the memory is ready to accept a new read request.
+    input  wire        i_imem_ready,
     // 32-bit read address for the instruction memory. This is expected to be
     // 4 byte aligned - that is, the two LSBs should be zero.
     output wire [31:0] o_imem_raddr,
-    // Instruction word fetched from memory, available on the same cycle.
+    // Issue a read request to the memory on this cycle. This should not be
+    // asserted if `i_imem_ready` is not asserted.
+    output wire        o_imem_ren,
+    // Indicates that a valid instruction word is being returned from memory.
+    input  wire        i_imem_valid,
+    // Instruction word fetched from memory, available sequentially some
+    // M cycles after a request (imem_ren) is issued.
     input  wire [31:0] i_imem_rdata,
+
     // Data memory accesses go through a separate read/write data memory (dmem)
     // that is shared between read (load) and write (stored). The port accepts
     // a 32-bit address, read or write enable, and mask (explained below) each
-    // cycle. Reads are combinational - values are available immediately after
-    // updating the address and asserting read enable. Writes occur on (and
-    // are visible at) the next clock edge.
+    // cycle.
     //
+    // The timing of the dmem interface is the same as the imem interface. See
+    // the documentation above.
+    //
+    // Indicates that the memory is ready to accept a new read or write request.
+    input  wire        i_dmem_ready,
     // Read/write address for the data memory. This should be 32-bit aligned
     // (i.e. the two LSB should be zero). See `o_dmem_mask` for how to perform
     // half-word and byte accesses at unaligned addresses.
@@ -40,8 +62,8 @@ module hart #(
     // When asserted, the memory will perform a write to the aligned address
     // `o_dmem_addr`. When asserted, the memory will write the bytes in
     // `o_dmem_wdata` (specified by the mask) to memory at the specified
-    // address on the next rising clock edge. It is illegal to assert this and
-    // `o_dmem_ren` on the same cycle.
+    // address. It is illegal to assert this and `o_dmem_ren` on the same
+    // cycle.
     output wire        o_dmem_wen,
     // The 32-bit word to write to memory when `o_dmem_wen` is asserted. When
     // write enable is asserted, the byte lanes specified by the mask will be
@@ -49,8 +71,8 @@ module hart #(
     // clock edge. The other byte lanes of the word will be unaffected.
     output wire [31:0] o_dmem_wdata,
     // The dmem interface expects word (32 bit) aligned addresses. However,
-    // WISC-25 supports byte and half-word loads and stores at unaligned and
-    // 16-bit aligned addresses, respectively. To support this, the access
+    // the processor supports byte and half-word loads and stores at unaligned
+    // and 16-bit aligned addresses, respectively. To support this, the access
     // mask specifies which bytes within the 32-bit word are actually read
     // from or written to memory.
     //
@@ -69,12 +91,14 @@ module hart #(
     // the value of the `sb` instruction left by 24 bits to place it in the
     // appropriate byte lane.
     output wire [ 3:0] o_dmem_mask,
+    // Indicates that a valid data word is being returned from memory.
+    input  wire        i_dmem_valid,
     // The 32-bit word read from data memory. When `o_dmem_ren` is asserted,
     // this will immediately reflect the contents of memory at the specified
     // address, for the bytes enabled by the mask. When read enable is not
     // asserted, or for bytes not set in the mask, the value is undefined.
     input  wire [31:0] i_dmem_rdata,
-	// The output `retire` interface is used to signal to the testbench that
+    // The output `retire` interface is used to signal to the testbench that
     // the CPU has completed and retired an instruction. A single cycle
     // implementation will assert this every cycle; however, a pipelined
     // implementation that needs to stall (due to internal hazards or waiting
@@ -120,28 +144,21 @@ module hart #(
     // writeback stage by this instruction. If rd is 5'd0, this field is
     // ignored and can be treated as a don't care.
     output wire [31:0] o_retire_rd_wdata,
+    output wire [31:0] o_retire_dmem_addr,
+    output wire [ 3:0] o_retire_dmem_mask,
+    output wire        o_retire_dmem_ren,
+    output wire        o_retire_dmem_wen,
+    output wire [31:0] o_retire_dmem_rdata,
+    output wire [31:0] o_retire_dmem_wdata,
     // The current program counter of the instruction being retired - i.e.
     // the instruction memory address that the instruction was fetched from.
     output wire [31:0] o_retire_pc,
     // the next program counter after the instruction is retired. For most
     // instructions, this is `o_retire_pc + 4`, but must be the branch or jump
     // target for *taken* branches and jumps.
-    output wire [31:0] o_retire_next_pc,
-
-    //other signals
-
-    //raw bytes recieved from memory
-    output wire [31:0] o_retire_dmem_addr,
-    output wire        o_retire_dmem_ren,
-    output wire        o_retire_dmem_wen,
-    output wire [ 3:0] o_retire_dmem_mask,
-    output wire [31:0] o_retire_dmem_wdata,
-    output  wire [31:0] o_retire_dmem_rdata
-
-`ifdef RISCV_FORMAL
-    ,`RVFI_OUTPUTS,
-`endif
+    output wire [31:0] o_retire_next_pc
 );
+
     //ALU Output signals
     wire eq;
     wire slt;
@@ -289,12 +306,13 @@ module hart #(
     // stall signals
     wire stall_IF;
     wire stall_ID;
+    wire stall_MEM;
     wire stall_pc;
  
     // pipeline reg resets
     wire rst_IF_ID = i_rst | effective_halted | stall_IF; // stall fetch -> nop into decode (IF/ID reg)
     wire rst_ID_EX = i_rst | stall_ID; // stall decode -> nop into execute (ID/EX reg)
-
+    wire rst_MEM_WB = i_rst | stall_MEM;
     // Forwarding mux signals
     wire [1:0] forward_A;
     wire [1:0] forward_B;
@@ -318,6 +336,8 @@ module hart #(
     assign o_retire_dmem_mask = MEM_WB_dmem_mask;
     assign o_retire_dmem_wdata = MEM_WB_dmem_wdata_aligned;
     assign o_retire_dmem_rdata = MEM_WB_dmem_rdata;
+    //new additons for delayed memory
+    assign o_imem_ren = i_imem_ready & ~o_retire_halt;
 
     // memory interfaces
     assign o_imem_raddr = PC;
@@ -355,22 +375,25 @@ module hart #(
 
 
     hazard_detector hazard_detector_state(
-        .IF_ID_rs1(IF_ID_rs1),
-        .IF_ID_rs2(IF_ID_rs2),
-        .IF_ID_format(IF_ID_format),
-        .ID_EX_format(ID_EX_format),
-        .ID_EX_mem_read(ID_EX_c_mem_read),
-        .ID_EX_write_reg(ID_EX_write_reg),
-        .EX_MEM_write_reg(EX_MEM_write_reg),
-        .c_is_jalr(c_is_jalr),
-        .ID_EX_c_is_jalr(ID_EX_c_is_jalr),
+        .i_IF_ID_rs1(IF_ID_rs1),
+        .i_IF_ID_rs2(IF_ID_rs2),
+        .i_IF_ID_format(IF_ID_format),
+        .i_ID_EX_format(ID_EX_format),
+        .i_ID_EX_mem_read(ID_EX_c_mem_read),
+        .i_ID_EX_write_reg(ID_EX_write_reg),
+        .i_EX_MEM_write_reg(EX_MEM_write_reg),
+        .i_c_is_jalr(c_is_jalr),
+        .i_ID_EX_c_is_jalr(ID_EX_c_is_jalr),
         .i_o_eq(eq),
         .i_o_slt(slt),
-        .ID_EX_funct3(ID_EX_funct3),
-        .stall_pc(stall_pc),
-        .stall_IF(stall_IF),
-        .stall_ID(stall_ID),
-        .EX_MEM_c_unsigned(EX_MEM_c_unsigned)
+        .i_EX_MEM_c_unsigned(EX_MEM_c_unsigned),
+        .i_ID_EX_funct3(ID_EX_funct3),
+        .i_imem_ready(i_imem_ready),
+        .i_dmem_valid(i_dmem_valid),
+        .o_stall_pc(stall_pc),
+        .o_stall_IF(stall_IF),
+        .o_stall_ID(stall_ID),
+        .o_stall_MEM(stall_MEM)
     );
 
     
@@ -563,7 +586,7 @@ module hart #(
                 EX_MEM_rs1_data,
                 EX_MEM_c_halted,
                 EX_MEM_op2} <= 0;
-        end else begin
+        end else if (~stall_MEM) begin
             EX_MEM_alu_out <= alu_out;
             EX_MEM_rs2_data <= (forward_B == 2'b01) ? EX_TO_EX_data :
                                 (forward_B == 2'b10) ? reg_write_data:
@@ -612,7 +635,7 @@ module hart #(
 
     //MEM/WB pipeline register logic
     always @(posedge i_clk) begin
-        if (i_rst) begin
+        if (rst_MEM_WB) begin
             {MEM_WB_mem_out,
                 MEM_WB_imm,
                 MEM_WB_pc_plus4,
--- code/rtl/hazard_detector.v
+++ code/rtl/hazard_detector.v
@@ -4,22 +4,26 @@
 // immediate from the incoming instruction word. It is a purely combinational
 // block that is expected to be embedded in the instruction decoder.
 module hazard_detector (
-  input wire [4:0] IF_ID_rs1,
-  input wire [4:0] IF_ID_rs2,
-  input wire [5:0] IF_ID_format,
-  input wire [5:0] ID_EX_format,
-  input wire [4:0] ID_EX_write_reg,
-  input wire [4:0] EX_MEM_write_reg,
-  input wire ID_EX_mem_read,
-  input wire c_is_jalr,
+  input wire [4:0] i_IF_ID_rs1,
+  input wire [4:0] i_IF_ID_rs2,
+  input wire [5:0] i_IF_ID_format,
+  input wire [5:0] i_ID_EX_format,
+  input wire [4:0] i_ID_EX_write_reg,
+  input wire [4:0] i_EX_MEM_write_reg,
+  input wire i_ID_EX_mem_read,
+  input wire i_c_is_jalr,
   input wire i_o_eq,
   input wire i_o_slt,
-  input wire EX_MEM_c_unsigned,
-  input wire [2:0] ID_EX_funct3,
-  input wire ID_EX_c_is_jalr,
-  output wire stall_pc,
-  output wire stall_IF,
-  output wire stall_ID
+  input wire i_EX_MEM_c_unsigned,
+  input wire [2:0] i_ID_EX_funct3,
+  input wire i_ID_EX_c_is_jalr,
+  //stall for memory latency
+  input wire i_imem_ready,
+  input wire i_dmem_valid,
+  output wire o_stall_pc,
+  output wire o_stall_IF,
+  output wire o_stall_ID,
+  output wire o_stall_MEM
 );
   localparam J_TYPE = 6'b100000;
   localparam B_TYPE = 6'b001000;
@@ -27,27 +31,28 @@ module hazard_detector (
   localparam R_TYPE = 6'b000001;
   localparam I_TYPE = 6'b000010;
 
-  wire Branch_taken = (ID_EX_format == B_TYPE) &
-    (ID_EX_funct3[0] ^ (ID_EX_funct3[2] ? i_o_slt : i_o_eq)); // same convenient logic as control unit for branch conditions
+  wire Branch_taken = (i_ID_EX_format == B_TYPE) &
+    (i_ID_EX_funct3[0] ^ (i_ID_EX_funct3[2] ? i_o_slt : i_o_eq)); // same convenient logic as control unit for branch conditions
 
   //detect load to use stalls and branch/jump stalls(need to remove stalls that will be fixed by forwarding)
 
   //input the alu output signals to the control unit, pipeline the funct3 and the instruction type,
   //then match the alu output signals to the funct3 condition and then stall and flush based on that
 
-  wire ID_control_flow = (IF_ID_format == J_TYPE) | (c_is_jalr); // if jalr, then we know for sure that it's a control flow instruction, so we can use the control signal directly instead of checking the instruction type
-  wire EX_control_flow = (ID_EX_format == J_TYPE) | (ID_EX_c_is_jalr) | (ID_EX_format == B_TYPE & Branch_taken); // same for EX stage
-  wire load_to_use_stall = (ID_EX_format == I_TYPE) & (ID_EX_mem_read) //check if instruction in execute is a load
-                          & ((ID_EX_write_reg == IF_ID_rs1) | (ID_EX_write_reg == IF_ID_rs2 & (IF_ID_format == R_TYPE | IF_ID_format == S_TYPE | IF_ID_format == B_TYPE)))
-                          & (ID_EX_write_reg != 0)
-                          & (IF_ID_format != 6'b111111);
+  wire ID_control_flow = (i_IF_ID_format == J_TYPE) | (i_c_is_jalr); // if jalr, then we know for sure that it's a control flow instruction, so we can use the control signal directly instead of checking the instruction type
+  wire EX_control_flow = (i_ID_EX_format == J_TYPE) | (i_ID_EX_c_is_jalr) | (i_ID_EX_format == B_TYPE & Branch_taken); // same for EX stage
+  wire load_to_use_stall = (i_ID_EX_format == I_TYPE) & (i_ID_EX_mem_read) //check if instruction in execute is a load
+                          & ((i_ID_EX_write_reg == i_IF_ID_rs1) | (i_ID_EX_write_reg == i_IF_ID_rs2 & (i_IF_ID_format == R_TYPE | i_IF_ID_format == S_TYPE | i_IF_ID_format == B_TYPE)))
+                          & (i_ID_EX_write_reg != 0)
+                          & (i_IF_ID_format != 6'b111111);
   //TODO: implement check for load to use stall
   
 
 
-  assign stall_IF = (ID_control_flow | EX_control_flow) & (~stall_ID | Branch_taken); // can't nop decode for a decode stall
-  assign stall_ID = load_to_use_stall | Branch_taken;
-  assign stall_pc = stall_IF | stall_ID | Branch_taken; // if branch taken, need to stall pc to prevent wrong instruction fetch
+  assign o_stall_IF = ((ID_control_flow | EX_control_flow) & (~o_stall_ID | Branch_taken)) | ~i_imem_ready; // can't nop decode for a decode stall
+  assign o_stall_ID = load_to_use_stall | Branch_taken;
+  assign o_stall_pc = o_stall_IF | o_stall_ID | Branch_taken; // if branch taken, need to stall pc to prevent wrong instruction fetch
+  assign o_stall_MEM = 0;//TODO
 endmodule
 
 `default_nettype wire
